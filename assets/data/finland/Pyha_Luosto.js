69553f0c8fa3c7dca2a1a16a35c5f0e6d177eb99
gh6361 <gledaho@outlook.com>
2026-08-11 11:09:27 -0700

    added more descriptions

--- assets/data/finland/Pyha_Luosto.js
+++ assets/data/finland/Pyha_Luosto.js
@@ -8,17 +8,24 @@ export const Pyha_Luosto = {
   images: [
     {
       src: `../assets/images/finland/Pyha_Luosto/reduced-6088-2.jpg`,
-      caption: "Open skies over Reykjavik.",
-      location: "Downtown Reykjavik, Iceland",
       date: "October 2023",
-      detailedDescription: "The crisp air and the endless blue sky made this afternoon unforgettable.",
+      detailedDescription: "The name of the national park 'Pyhä-Luosto' \
+      is a cominbination of the two main fell areas: <em>Pyhätunturi</em> \
+      and <em>Luostotunturi</em> (<em>tunturi</em> translates as <em>fell</em>). \
+      <em>Pyhä</em> means <em>holy</em> or <em>sacred</em>, \
+      as it was sacred to the local Forest Sámi. The 35 km chain of fells is a \
+      remnant of one of the oldest mountain ranges in the world, formed around \
+      2 billion years ago. The park also contains Finland's deepest gorge, \
+      Isokuru, which reaches roughly 220 m deep."
     },
     {
       src: `../assets/images/finland/Pyha_Luosto/reduced-6163.jpg`,
-      caption: "Open skies over Reykjavik.",
-      location: "Downtown Reykjavik, Iceland",
       date: "October 2023",
-      detailedDescription: "The crisp air and the endless blue sky made this afternoon unforgettable.",
+      detailedDescription: "The Tunturiaapa Nature Trail goes through both \
+      old-growth forest and the open Tunturiaapa aapa mire. The trees in the \
+      old-growth forest are generally over 250 years, although some are up to 400 \
+      years old. In contrast, an aapa mire is a fen-type peatland. The image \
+      depicts a forested part of the trail.",
     },
   ]
 };
--- assets/data/finland/swing.js
+++ assets/data/finland/swing.js
@@ -9,13 +9,20 @@ export const swing = {
     {
       src: `../assets/images/finland/swing/reduced-6123.jpg`,
       date: "October 2023",
-      detailedDescription: "Wouldn't it be nice to walk out onto that terrace \
-      every morning? And the high ceilings!",
+      detailedDescription: "Lake Äkäslompolo is situated in Finnish \
+      Lapland and lies completely above the Arctic Circle. It is surrounded by forest \
+      and fells of the Ylläs region, the latter of which is a rounded, treeless or sparsely \
+      wooded mountain or hill, typical of northern Scandinavia.",
     },
     {
       src: `../assets/images/finland/swing/reduced-6127.jpg`,
       date: "October 2023",
-      detailedDescription: "The Ylläs Swing, situated on Lake Äkäslompolo.",
+      detailedDescription: "The Ylläs Swing, situated on Lake Äkäslompolo. \
+      The words on top of the swing <em>Ylläksen Yöpuu</em> directly translate from \
+      Finnish as <em>Ylläs's night tree</em>. <em>Yöpuu</em> can also evoke the idea of a \
+      nighttime resting or perching place, especially for birds (imagine the \
+      place in winter, under the Northern Lights!). \
+      Ylläs is the fell in the background.",
     },
     {
       src: `../assets/images/finland/swing/reduced-6153.jpg`,
